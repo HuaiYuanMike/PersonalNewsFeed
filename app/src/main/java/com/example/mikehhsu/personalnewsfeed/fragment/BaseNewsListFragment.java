fba206bd56d75a048e7b717a3dff3ca7f6d7bc65
HuaiYuanMike <mikepavelhsu@gmail.com>
2016-08-30 21:32:37 -0400

    Added a RecyclerView widget to the fragment_news_list.xml. Implementing the adapter to the RecyclerView

--- app/src/main/java/com/example/mikehhsu/personalnewsfeed/fragment/BaseNewsListFragment.java
+++ app/src/main/java/com/example/mikehhsu/personalnewsfeed/fragment/BaseNewsListFragment.java
@@ -3,12 +3,14 @@ package com.example.mikehhsu.personalnewsfeed.fragment;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.mikehhsu.personalnewsfeed.R;
 import com.example.mikehhsu.personalnewsfeed.activity.MainActivity;
-
 /**
  * Created by mikehhsu on 8/2/16.
  */
@@ -46,5 +48,31 @@ public class BaseNewsListFragment extends BaseFragment {
     public void onStart() {
         super.onStart();
         ((TextView)getView().findViewById(R.id.title_temp)).setText(newsListType.getTitle());
+    }
+
+    private class NewsListRecyclerAdapter extends RecyclerView.Adapter<NewsListRecyclerAdapter.ViewHolder>{
+
+        public class ViewHolder extends RecyclerView.ViewHolder{
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            //create new viewholder
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+        //update data of each item to the viewholder
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
